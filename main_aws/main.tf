6eacc603e63ab510336cd5109d761655c26cb35a
jake <jakeschuler@arizona.edu>
2023-11-29 17:27:09 -0700

    Fixed error with database.tf

--- main_aws/main.tf
+++ main_aws/main.tf
@@ -8,32 +8,35 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0" #TODO pick version of aws provider
+      version = ">= 3.0"
     }
   }
 }
 
-
+variable "db_pass" {
+  type = string
+  sensitive = true
+}
 module "aws_module" {
   source = "../aws"
-  region = "us-east-1"
-  instance_type = "t2.micro"
-  db_instance_type = "db.t2.micro"
-  elasti_cache_instance_type = "cache.t2.micro"
-  ami = "ami-0fc5d935ebf8bc3bc"
-  domain = "jdschuler.com"
-  sub_domain = "www"
-  min_elb_capacity = 2
-  max_elb_capacity = 5
-  desired_elb_capacity = 3
-  database_storage = 20
-  backup_retention_period = 7
-  num_cache_nodes = 1
-  availability_zones = ["us-east-1a", "us-east-1b"]
-  bucket_name = "my-elb-logs-bucket"
-  db_name = "default_username"
-  key_pair_name = "my-key-pair"
-  db_pass = "default"
-  waf_rate_limit = 1000
+  region = "us-east-1" # Replace with your region
+  instance_type = "t2.micro" # Replace with your instance type
+  db_instance_type = "db.t2.micro" # Replace with your instance type
+  elasti_cache_instance_type = "cache.t2.micro" # Replace with your instance type
+  ami = "ami-0fc5d935ebf8bc3bc" # Replace with your ami
+  domain = "example.com" # Replace with your domain
+  sub_domain = "www"  # Replace with your sub domain
+  min_elb_capacity = 2 # Replace with your min capacity
+  max_elb_capacity = 5 # Replace with your max capacity
+  desired_elb_capacity = 3 # Replace with your desired capacity
+  database_storage = 20 # Replace with your database storage
+  backup_retention_period = 7 # Replace with your backup retention period
+  num_cache_nodes = 1 # Replace with your number of cache nodes
+  availability_zones = ["us-east-1a", "us-east-1b"] # Replace with your availability zones
+  bucket_name = "my-elb-logs-bucket" # Replace with your bucket name
+  db_name = "default_username" # Replace with your database name
+  db_pass = var.db_pass
+  key_pair_name = "my-key-pair" # Replace with your key pair name
+  waf_rate_limit = 1000 # Replace with your rate limit
   ssh_ip_address = ["0.0.0.0/0"] # Add your ip address here
 }
