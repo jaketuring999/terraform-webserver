866c0eb804252a294ed47497afdc2633f655a7ef
jake <jakeschuler@arizona.edu>
2023-11-29 23:19:39 -0700

    Add private key output

--- aws/keypair.tf
+++ aws/keypair.tf
@@ -7,3 +7,8 @@ resource "aws_key_pair" "generated_key" {
   key_name   = var.key_pair_name
   public_key = tls_private_key.key_pair.public_key_openssh
 }
+
+output "private_key" {
+  value = tls_private_key.key_pair.private_key_pem
+  sensitive = true
+}
